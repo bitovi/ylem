c76907760be16550148c1cfb11bc31cc23073cf9
Adam Barrett <adamlbarrett@hotmail.com>
2016-08-24 01:16:49 -0500

    Support can-define/map as a viewModel
     - import can-define and can-compute instead of can
     - accept a can-define/map constructor function as a mapToProps argument
     - create props for connected component by serializing viewModel and copying bound methods over

--- src/can-react.js
+++ src/can-react.js
@@ -1,27 +1,46 @@
 import React from 'react';
-import compute from 'can/compute/compute';
+import compute from 'can-compute';
+import DefineMap from "can-define/map/";
 
-export function connect(mapToProps, ComponentToConnect) {
-  const connectDisplayName = `Connected(${ getDisplayName(ComponentToConnect) })`;
-
-  const mapToState = (props) => {
-    return Object.assign( {}, props, mapToProps(props) );
-  };
+export function connect( MapToProps, ComponentToConnect ) {
 
   class ConnectedComponent extends React.Component {
 
     constructor(props) {
       super(props);
       this.propsCompute = compute(props);
-      this.compute = compute(() => {
-        return mapToState( this.propsCompute() );
-      });
-      this.state = this.compute();
+
+      if ( MapToProps.prototype instanceof DefineMap ) {
+        this.viewModel = new MapToProps( this.propsCompute() );
+        this.mapToState = this.createMapToStateWithViewModel( this.viewModel );
+      } else {
+        this.mapToState = this.createMapToStateWithFunction( MapToProps )
+      }
+
+      this.state = { propsForChild: this.mapToState() };
       let batchNum;
-      this.compute.bind("change", (ev, newVal) => {
+      this.mapToState.bind("change", (ev, newVal) => {
         if(!ev.batchNum || ev.batchNum !== batchNum) {
-          this.setState(newVal);
+          this.setState({ propsForChild: newVal });
         }
+      });
+    }
+
+    createMapToStateWithViewModel( vm ) {
+      return compute(() => {
+        vm.set( this.propsCompute() );
+        const props = vm.serialize();
+        getMethodNames( vm ).forEach( methodName => {
+          props[methodName] = vm[methodName].bind(vm);
+        });
+        return props;
+      });
+    }
+
+    createMapToStateWithFunction( func ) {
+      return compute(() => {
+        const props = this.propsCompute();
+        return Object.assign( {}, props, func(props) );
       });
     }
 
@@ -30,16 +49,30 @@ export function connect(mapToProps, ComponentToConnect) {
     }
 
     render() {
-      return React.createElement(ComponentToConnect, this.state);
+      return React.createElement(ComponentToConnect, this.state.propsForChild);
     }
 
   }
 
-  ConnectedComponent.displayName = connectDisplayName;
+  ConnectedComponent.displayName = `Connected(${ getDisplayName(ComponentToConnect) })`;
 
   return ConnectedComponent;
 }
 
 function getDisplayName(ComponentToConnect) {
   return ComponentToConnect.displayName || ComponentToConnect.name || 'Component';
+}
+
+function getMethodNames( obj ) {
+  const result = [];
+  for (var key in obj) {
+    try {
+      if (typeof( obj[key] ) == "function") {
+        result.push( key );
+      }
+    } catch (err) {
+      // do nothing
+    }
+  }
+  return result;
 }
--- src/test/test.js
+++ src/test/test.js
@@ -1,8 +1,9 @@
 import chai from 'chai';
 import { connect } from '../can-react';
 import React from 'react';
-import compute from 'can/compute/';
+import compute from 'can-compute';
 import ReactTestUtils from 'react-addons-test-utils';
+import DefineMap from "can-define/map/";
 import 'steal-mocha';
 const assert = chai.assert;
 
@@ -21,53 +22,127 @@ describe('can-react', function(){
       shallowRenderer = ReactTestUtils.createRenderer();
     });
 
-    it('should not (by default) render additional dom nodes that the ones from the extended Presentational Component', () => {
+    describe('with a map to props function', () => {
 
-      const ConnectedComponent = connect( () => ({ value: 'bar' }), TestComponent );
+      it('should not (by default) render additional dom nodes that the ones from the extended Presentational Component', () => {
 
-      shallowRenderer.render( React.createElement( ConnectedComponent, { value: 'foo' } ) );
+        const ConnectedComponent = connect( () => ({ value: 'bar' }), TestComponent );
 
-      const result = shallowRenderer.getRenderOutput();
+        shallowRenderer.render( React.createElement( ConnectedComponent, { value: 'foo' } ) );
 
-      assert.equal(result.type, TestComponent); // not ConnectedComponent
-      assert.equal(result.props.value, 'bar'); // not 'foo'
+        const result = shallowRenderer.getRenderOutput();
+
+        assert.equal(result.type, TestComponent); // not ConnectedComponent
+        assert.equal(result.props.value, 'bar'); // not 'foo'
+
+      });
+
+      it('should update the component whenever an observable read inside the mapToProps function emits a change event', function() {
+        const observable = compute('Inital Value');
+        const ConnectedComponent = connect( () => ({ value: observable() }), TestComponent );
+
+        const result = ReactTestUtils.renderIntoDocument( React.createElement( ConnectedComponent ) );
+        const childComponent = ReactTestUtils.scryRenderedComponentsWithType(result, TestComponent)[0];
+
+        assert.equal(childComponent.props.value, 'Inital Value');
+        observable('new value');
+        assert.equal(childComponent.props.value, 'new value');
+
+      });
+
+      it('should update the component when new props are recieved', function() {
+        const observable = compute('Inital Observable Value');
+        const ConnectedComponent = connect( ({ propValue }) => ({ value: observable(), propValue }), TestComponent );
+        const WrappingComponent = React.createClass({
+          getInitialState() {
+            return { propValue: 'Initial Prop Value' };
+          },
+          changeState() {
+            this.setState( { propValue: 'New Prop Value' } );
+          },
+          render() {
+            return <ConnectedComponent propValue={ this.state.propValue } />;
+          }
+        });
+
+        const result = ReactTestUtils.renderIntoDocument( React.createElement( WrappingComponent ) );
+        const childComponent = ReactTestUtils.scryRenderedComponentsWithType(result, TestComponent)[0];
+
+        assert.equal(childComponent.props.propValue, 'Initial Prop Value');
+        result.changeState();
+        assert.equal(childComponent.props.propValue, 'New Prop Value');
+      });
 
     });
 
-    it('should update the component whenever an observable read inside the mapToProps function emits a change event', function() {
-      const observable = compute('Inital Value');
-      const ConnectedComponent = connect( () => ({ value: observable() }), TestComponent );
+    describe('with can-define constructor function', () => {
 
-      const result = ReactTestUtils.renderIntoDocument( React.createElement( ConnectedComponent ) );
-      const childComponent = ReactTestUtils.scryRenderedComponentsWithType(result, TestComponent)[0];
-
-      assert.equal(childComponent.props.value, 'Inital Value');
-      observable('new value');
-      assert.equal(childComponent.props.value, 'new value');
-
-    });
-
-    it('should update the component when new props are recieved', function() {
-      const observable = compute('Inital Observable Value');
-      const ConnectedComponent = connect( ({ propValue }) => ({ value: observable(), propValue }), TestComponent );
-      const WrappingComponent = React.createClass({
-        getInitialState() {
-          return { propValue: 'Initial Prop Value' };
+      const DefinedViewModel = DefineMap.extend({
+        foo: {
+          type: 'string',
+          value: 'foo'
         },
-        changeState() {
-          this.setState( { propValue: 'New Prop Value' } );
+        bar: 'string',
+        foobar: {
+          get() {
+            return this.foo + this.bar;
+          },
+          serialize: true
         },
-        render() {
-          return <ConnectedComponent propValue={ this.state.propValue } />;
+        callback() {
+          return this;
         }
       });
 
-      const result = ReactTestUtils.renderIntoDocument( React.createElement( WrappingComponent ) );
-      const childComponent = ReactTestUtils.scryRenderedComponentsWithType(result, TestComponent)[0];
+      it('should assign a property to the component called `viewModel` with an instance of ViewModel as the value', () => {
+        const ConnectedComponent = connect( DefinedViewModel, TestComponent );
+        const result = ReactTestUtils.renderIntoDocument( React.createElement( ConnectedComponent ) );
+        assert.ok( result.viewModel instanceof DefinedViewModel );
+      });
 
-      assert.equal(childComponent.props.propValue, 'Initial Prop Value');
-      result.changeState();
-      assert.equal(childComponent.props.propValue, 'New Prop Value');
+      it('should pass a props object with copied methods, that have the correct context (the viewmodel) to be used as callbacks', () => {
+        const ConnectedComponent = connect( DefinedViewModel, TestComponent );
+        const result = ReactTestUtils.renderIntoDocument( React.createElement( ConnectedComponent ) );
+        const childComponent = ReactTestUtils.scryRenderedComponentsWithType(result, TestComponent)[0];
+        assert.equal(childComponent.props.callback(), result.viewModel);
+      });
+
+      it('should update whenever any observable property on the viewModel instance changes', function() {
+
+        const ConnectedComponent = connect( DefinedViewModel, TestComponent );
+
+        const result = ReactTestUtils.renderIntoDocument( React.createElement( ConnectedComponent, { bar: 'bar', baz: 'bam' } ) );
+        const childComponent = ReactTestUtils.scryRenderedComponentsWithType( result, TestComponent )[0];
+
+        assert.equal(childComponent.props.foobar, 'foobar');
+        result.viewModel.foo = 'MMM';
+        assert.equal(childComponent.props.foobar, 'MMMbar');
+
+      });
+
+      it('should update the component when new props are recieved', function() {
+        const observable = compute('Inital Observable Value');
+        const ConnectedComponent = connect( DefinedViewModel, TestComponent );
+        const WrappingComponent = React.createClass({
+          getInitialState() {
+            return { propValue: 'Initial Prop Value' };
+          },
+          changeState() {
+            this.setState( { propValue: 'New Prop Value' } );
+          },
+          render() {
+            return <ConnectedComponent propValue={ this.state.propValue } />;
+          }
+        });
+
+        const result = ReactTestUtils.renderIntoDocument( React.createElement( WrappingComponent ) );
+        const childComponent = ReactTestUtils.scryRenderedComponentsWithType(result, TestComponent)[0];
+
+        assert.equal(childComponent.props.propValue, 'Initial Prop Value');
+        result.changeState();
+        assert.equal(childComponent.props.propValue, 'New Prop Value');
+      });
+
     });
 
   });
