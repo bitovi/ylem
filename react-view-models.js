90e27e3a3aef5c3bc19686922a13fc8fe65e9b81
Christopher Baker <christopher@bitovi.com>
2017-05-09 11:46:55 -0700

    add override checks

--- react-view-models.js
+++ react-view-models.js
@@ -14,6 +14,42 @@ export default class CanReactComponent extends React.Component {
     this.render = compute(function() {
       return this._render();
     }, this);
+
+    { // TODO: Remove in PROD
+      let methodAsString = null;
+
+      methodAsString = this.componentWillMount.toString();
+      if (
+        this.componentWillMount !== CanReactComponent.prototype.componentWillMount
+        && !methodAsString.includes('componentWillMount', methodAsString.indexOf(') {'))
+      ) {
+        throw new Error(`super.componentWillMount() must be called on ${ this.constructor.name }.`);
+      }
+
+      methodAsString = this.componentWillUnmount.toString();
+      if (
+        this.componentWillUnmount !== CanReactComponent.prototype.componentWillUnmount
+        && !methodAsString.includes('componentWillUnmount', methodAsString.indexOf(') {'))
+      ) {
+        throw new Error(`super.componentWillUnmount() must be called on ${ this.constructor.name }.`);
+      }
+
+      methodAsString = this.componentWillReceiveProps.toString();
+      if (
+        this.componentWillReceiveProps !== CanReactComponent.prototype.componentWillReceiveProps
+        && !methodAsString.includes('componentWillReceiveProps', methodAsString.indexOf(') {'))
+      ) {
+        throw new Error(`super.componentWillReceiveProps() must be called on ${ this.constructor.name }.`);
+      }
+
+      methodAsString = this.shouldComponentUpdate.toString();
+      if (
+        this.shouldComponentUpdate !== CanReactComponent.prototype.shouldComponentUpdate
+        && !methodAsString.includes('shouldComponentUpdate', methodAsString.indexOf(') {'))
+      ) {
+        throw new Error(`super.shouldComponentUpdate() must be called on ${ this.constructor.name }.`);
+      }
+    }
   }
 
   get props() {
