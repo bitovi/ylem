9cea1817b1d11c24d3e3d0f1feebad82643e5de4
Christopher Baker <christopher@bitovi.com>
2017-05-23 13:58:33 -0700

    fix build

--- build.js
+++ build.js
@@ -9,33 +9,4 @@ stealTools.export( {
 		"+amd": {},
 		"+global-js": {}
 	}
-} )
-  .then(function() {
-	return stealTools.export( {
-		steal: {
-			main: 'test/test',
-			config: __dirname + "/package.json!npm"
-		},
-		options: {
-			ignoreAllDependencies: true,
-			format: 'cjs',
-			sourceMaps: true
-		},
-		outputs: {
-			"test": {
-				modules: ['test/test'],
-				dest: function(){
-					return __dirname + '/dist/test/test.js';
-				},
-				format: 'cjs'
-			}
-		}
-	} );
-})
-  .catch( function( e ) {
-
-	setTimeout( function() {
-		throw e;
-	}, 1 );
-
 } );
