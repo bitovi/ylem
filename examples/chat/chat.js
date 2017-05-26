5249732150b1089ce318b6d670b1487a23b7e1cb
Christopher Baker <christopher@hmudesign.com>
2017-05-25 20:17:40 -0700

    add chat example

---
+++ examples/chat/chat.js
@@ -0,0 +1,51 @@
+import React from "react";
+import DefineMap from "can-define/map/";
+import reactViewModel from "react-view-models";
+import route from "can-route";
+import { routeUrl } from "can-stache/helpers/route";
+import Messages from "./messages";
+
+const AppVM = DefineMap.extend({
+	route: "string",
+	page: "string",
+	message: {
+		type: "string",
+		value: "Chat Home",
+		serialize: false
+	},
+	addExcitement: function() {
+		this.message = this.message + "!";
+	}
+});
+
+const appVM = new AppVM();
+
+route.data = appVM;
+route("{page}", { page: "home" });
+route.ready();
+
+const template = reactViewModel(AppVM, (props) => (
+	<div className="container">
+		<div className="row">
+			<div className="col-sm-8 col-sm-offset-2">
+				{ props.page === "home" ? (
+					<div>
+						<h1
+							className="page-header text-center"
+							onClick={ () => props.addExcitement() }
+						>{props.message}</h1>
+						<a
+							href={routeUrl({ page: "chat" })}
+							className="btn btn-primary btn-block btn-lg"
+						>Start chat</a>
+					</div>
+				) : (
+					<Messages />
+				) }
+			</div>
+		</div>
+	</div>
+));
+
+const frag = template(appVM);
+document.body.appendChild(frag);
---
+++ examples/chat/messages.js
@@ -0,0 +1,91 @@
+import React from "react";
+import DefineMap from "can-define/map/";
+import { Component } from "react-view-models";
+import { promise } from "react-view-models/helpers/";
+import { routeUrl } from "can-stache/helpers/route";
+import Message from "./models/message";
+
+export default class Messages extends Component {
+	render() {
+		return (
+			<div>
+				<h1 className="page-header text-center">Chat Messages</h1>
+				<h5><a href={routeUrl({ page: "home" })}>Home</a></h5>
+
+				{ this.props.messagesPromise.isPending ? (
+					<div className="list-group-item list-group-item-info">
+						<h4 className="list-group-item-heading">Loading...</h4>
+					</div>
+				) : null }
+
+				{ this.props.messagesPromise.isRejected ? (
+					<div className="list-group-item list-group-item-danger">
+						<h4 className="list-group3--item-heading">Error</h4>
+						<p className="list-group-item-text">{this.props.messagesPromise.reason}</p>
+					</div>
+				) : null }
+
+				{ this.props.messagesPromise.isResolved ? (
+					this.props.messagesPromise.value ? (
+						this.props.messagesPromise.value.map(({ name, body }, key) => (
+							<div className="list-group-item" key={key}>
+								<h4 className="list-group3--item-heading">{name}</h4>
+								<p className="list-group-item-text">{body}</p>
+							</div>
+						)).serialize() // TODO: iterable DefineList means no .serialize()
+					) : (
+						<div className="list-group-item">
+							<h4 className="list-group-item-heading">No messages</h4>
+						</div>
+					)
+				) : null }
+
+				<form className="row" onSubmit={ (ev) => this.props.send(ev) }>
+					<div className="col-sm-3">
+						<input
+							type="text"
+							className="form-control"
+							placeholder="Your name"
+							value={this.props.name}
+							onChange={ (e) => this.props.name = e.target.value }
+						/>
+					</div>
+					<div className="col-sm-6">
+						<input
+							type="text"
+							className="form-control"
+							placeholder="Your message"
+							value={this.props.body}
+							onChange={ (e) => this.props.body = e.target.value }
+						/>
+					</div>
+					<div className="col-sm-3">
+						<input type="submit" className="btn btn-primary btn-block" value="Send" />
+					</div>
+				</form>
+			</div>
+		);
+	}
+}
+
+Messages.ViewModel = DefineMap.extend({
+	messagesPromise: promise(() => Message.getList({})),
+	name: {
+		type: "string",
+		value: ""
+	},
+	body: {
+		type: "string",
+		value: ""
+	},
+	send: function(event) {
+		event.preventDefault();
+
+		new Message({
+			name: this.name,
+			body: this.body
+		}).save().then(() => {
+			this.body = "";
+		});
+	}
+});
