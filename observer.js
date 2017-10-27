cfaf391a66a0ad8b088ef335f1817f0802415a80
Christopher Baker <christopher@bitovi.com>
2017-10-27 15:35:51 -0700

    update observer to use can-observation-recorder

--- observer.js
+++ observer.js
@@ -1,42 +1,60 @@
-var Observation = require("can-observation");
-var assign = require("can-util/js/assign/assign");
+var canReflect = require("can-reflect");
+var ObservationRecorder = require("can-observation-recorder");
+var recorderHelpers = require("can-observation/recorder-dependency-helpers");
+var queues = require("can-queues");
 
-function Observer() {
+var ORDER = undefined;
+
+function Observer(onUpdate) {
+	this.newDependencies = ObservationRecorder.makeDependenciesRecorder();
+	this.oldDependencies = null;
+	this.onUpdate = onUpdate;
+
 	var self = this;
-	Observation.call(self, null, null, function() { return self.listener && self.listener(); });
+	this.onDependencyChange = function(newVal, oldVal) {
+		self.dependencyChange(this, newVal, oldVal);
+	};
 }
 
-Observer.prototype = Object.create(Observation.prototype);
-Observer.prototype.constructor = Observer;
+Observer.prototype.startRecording = function() {
+	this.oldDependencies = this.newDependencies;
+	ObservationRecorder.start();
 
-assign(Observer.prototype, {
-	start: function() {
-		this.value = {};
-	},
-	startLisening: function(listener) {
-		this.listener = listener || this.listener;
-
-		this.bound = true;
-		this.oldObserved = this.newObserved || {};
-		this.ignore = 0;
-		this.newObserved = {};
-
-		Observation.observationStack.push(this);
-	},
-	stopListening: function() {
-		if (Observation.observationStack[Observation.observationStack.length - 1] !== this) {
-			var index = Observation.observationStack.indexOf(this);
-			if (index === -1) {
-				throw new Error("Async observations stopped out of order.");
-			}
-
-			Observation.observationStack.splice(index, 1);
-			Observation.observationStack.push(this);
+	if(this.order !== undefined) {
+		ORDER = this.order;
+	}
+	else {
+		if(ORDER !== undefined) {
+			this.order = ++ORDER;
 		}
+		else {
+			// the root component
+			this.order = ORDER = 0;
+		}
+	}
 
-		Observation.observationStack.pop();
-		this.updateBindings();
+	// console.log(canReflect.getName(this), this.order)
+};
+
+Observer.prototype.stopRecording = function() {
+	this.newDependencies = ObservationRecorder.stop();
+	recorderHelpers.updateObservations(this);
+};
+
+Observer.prototype.dependencyChange = function() {
+	queues.deriveQueue.enqueue(this.onUpdate, this, [], { priority: this.order });
+};
+
+Observer.prototype.teardown = function() {
+	queues.priorityQueue.dequeue(this.onUpdate);
+};
+
+//!steal-remove-start
+canReflect.assignSymbols(Observer.prototype, {
+	"can.getName": function() {
+		return canReflect.getName(this.constructor) + "<" + canReflect.getName(this.onUpdate) + ">";
 	},
 });
+//!steal-remove-end
 
 module.exports = Observer;
