a17ab9c6f0417071f375c72f6334a6346c3c08a8
Christopher Baker <christopher@bitovi.com>
2017-10-31 08:52:01 -0700

    0.5.11

---
+++ dist/amd/component.js
@@ -0,0 +1,79 @@
+/*react-view-model@0.5.10#component*/
+define([
+    'require',
+    'exports',
+    'module',
+    'react',
+    'can-define/map',
+    'can-util/js/assign',
+    './observer',
+    './helpers/make-enumerable',
+    './helpers/autobind-methods',
+    'can-util/js/dev',
+    'can-namespace'
+], function (require, exports, module) {
+    var React = require('react');
+    var DefineMap = require('can-define/map');
+    var assign = require('can-util/js/assign');
+    var Observer = require('./observer');
+    var makeEnumerable = require('./helpers/make-enumerable');
+    var autobindMethods = require('./helpers/autobind-methods');
+    var dev = require('can-util/js/dev');
+    var namespace = require('can-namespace');
+    if (React) {
+        var Component = function Component() {
+            React.Component.call(this);
+            if (this.constructor.ViewModel) {
+                autobindMethods(this.constructor.ViewModel, true);
+                makeEnumerable(this.constructor.ViewModel, true);
+            }
+            this._observer = new Observer();
+            if (typeof this.shouldComponentUpdate === 'function') {
+                this._shouldComponentUpdate = this.shouldComponentUpdate;
+            }
+            this.shouldComponentUpdate = function () {
+                return false;
+            };
+        };
+        Component.prototype = Object.create(React.Component.prototype);
+        assign(Component.prototype, {
+            constructor: Component,
+            componentWillReceiveProps: function (nextProps) {
+                var props = {};
+                for (var key in nextProps) {
+                    if (!(key in this.props) || nextProps[key] !== this.props[key]) {
+                        props[key] = nextProps[key];
+                    }
+                }
+                this.viewModel.assign(props);
+            },
+            componentWillMount: function () {
+                var ViewModel = this.constructor.ViewModel || DefineMap;
+                this.viewModel = new ViewModel(this.props);
+                this._observer.startLisening(function () {
+                    if (typeof this._shouldComponentUpdate !== 'function' || this._shouldComponentUpdate()) {
+                        this.forceUpdate();
+                    }
+                }.bind(this));
+            },
+            componentDidMount: function () {
+                this._observer.stopListening();
+            },
+            componentWillUpdate: function () {
+                this._observer.startLisening();
+            },
+            componentDidUpdate: function () {
+                this._observer.stopListening();
+            },
+            componentWillUnmount: function () {
+                this._observer.stop();
+                this.viewModel = null;
+            }
+        });
+        module.exports = namespace.ReactViewModelComponent = Component;
+    } else {
+        module.exports = namespace.ReactViewModelComponent = function Component() {
+            throw new Error('You must provide React before can.all.js');
+        };
+    }
+});
---
+++ dist/amd/helpers/make-enumerable.js
@@ -0,0 +1,48 @@
+/*react-view-model@0.5.10#helpers/make-enumerable*/
+define([
+    'require',
+    'exports',
+    'module',
+    'can-util/js/each'
+], function (require, exports, module) {
+    var each = require('can-util/js/each');
+    module.exports = function makeEnumerable(Type, recursive) {
+        if (isEnumerable(Type)) {
+            return;
+        }
+        if (recursive === undefined) {
+            recursive = true;
+        }
+        var setup = Type.prototype.setup;
+        Type.prototype.setup = function () {
+            if (this._define) {
+                var map = this;
+                each(this._define.definitions, function (value, prop) {
+                    var descriptor = Object.getOwnPropertyDescriptor(map.constructor.prototype, prop);
+                    descriptor.enumerable = true;
+                    Object.defineProperty(map, prop, descriptor);
+                    if (recursive && value.Type) {
+                        makeEnumerable(value.Type, recursive);
+                    }
+                });
+                each(this._define.methods, function (method, prop) {
+                    if (prop === 'constructor') {
+                        return;
+                    }
+                    var descriptor = Object.getOwnPropertyDescriptor(map.constructor.prototype, prop);
+                    descriptor.enumerable = true;
+                    Object.defineProperty(map, prop, descriptor);
+                });
+            }
+            return setup.apply(this, arguments);
+        };
+        Object.defineProperty(Type, '__isEnumerable', {
+            enumerable: false,
+            value: true
+        });
+    };
+    function isEnumerable(Type) {
+        return !!Type.__isEnumerable;
+    }
+    module.exports.isEnumerable = isEnumerable;
+});
